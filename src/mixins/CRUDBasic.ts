8ad41a4c3dbc4a4c98c55e6fe746087a335adff2
Aditya Kresna Permana <slaveofcode@gmail.com>
2019-10-10 14:17:31 +0700

    Added try catch on get detail and update

--- src/mixins/CRUDBasic.ts
+++ src/mixins/CRUDBasic.ts
@@ -2,41 +2,53 @@ import PageNotFoundException from '@exceptions/PageNotFound';
 import { Model } from 'sequelize';
 
 export default class {
-  protected entityName: string | undefined;
+  protected entityName: string;
   protected crudClass: any;
 
   protected async list(): Promise<any[]> {
-    const results = await this.crudClass.findAll({ raw: true });
-    return results;
+    try {
+      const results = await this.crudClass.findAll({ raw: true });
+      return results;
+    } catch {
+      return [];
+    }
   }
 
   protected async get(id: number): Promise<any> {
-    const item = await this.crudClass.findOne({
-      where: { id },
-      raw: true,
-    });
-    return item;
+    try {
+      const item = await this.crudClass.findOne({
+        where: { id },
+        raw: true,
+      });
+      return item;
+    } catch {
+      return null;
+    }
   }
 
   protected async update(id: number, updateParams: any)
     : Promise<[boolean, any] | PageNotFoundException> {
-    const item = await this.crudClass.findOne({
-      where: { id },
-      raw: true,
-    });
+    try {
+      const item = await this.crudClass.findOne({
+        where: { id },
+        raw: true,
+      });
 
-    if (!item) {
-      return new PageNotFoundException(`${this.entityName} not found`);
+      if (!item) {
+        return new PageNotFoundException(`${this.entityName} not found`);
+      }
+
+      const [affectedRowsNum, updatedItem] = await this.crudClass.update(updateParams, {
+        where: { id },
+        returning: true,
+      });
+
+      const isUpdated = affectedRowsNum > 0;
+
+      return [isUpdated, isUpdated ? updatedItem[0] : null];
+    } catch {
+      return [false, null];
     }
-
-    const [affectedRowsNum, updatedItem] = await this.crudClass.update(updateParams, {
-      where: { id },
-      returning: true,
-    });
-
-    const isUpdated = affectedRowsNum > 0;
-
-    return [isUpdated, isUpdated ? updatedItem[0] : null];
   }
 
   protected async create<M extends Model>(createParams: any): Promise<M | boolean> {
